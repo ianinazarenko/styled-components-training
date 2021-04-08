023bde28e5b67e2bf5ab11aa277e051d2d0c0f20
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-04-08 19:54:53 +0300

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: red;
+  background: blue;
+  text-transform: uppercase;
+`;
 
 function App() {
-  return <div className="App">Styled components</div>;
+  return (
+    <div className='App'>
+      Styled components
+      <Button>click me</Button>
+    </div>
+  );
 }
 
 export default App;
