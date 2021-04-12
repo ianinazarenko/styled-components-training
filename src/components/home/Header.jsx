12196c50b71f1b354dd3ab361f3a61ed2874c2ff
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-04-12 13:54:59 +0300

    Created PrimaryButton component

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../globals/Hero';
 import aboutImg from '../../images/aboutBcg.jpeg';
 import Banner from '../globals/Banner';
+import { PrimaryButton } from '../globals/Buttons';
 
 function Header() {
   return (
@@ -9,8 +10,9 @@ function Header() {
       <Banner
         greeting='welcome to'
         title='beachwalk resort'
-        text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quis repudiandae totam voluptates amet iusto.'
-      />
+        text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quis repudiandae totam voluptates amet iusto.'>
+        <PrimaryButton t='1rem'>View details</PrimaryButton>
+      </Banner>
     </Hero>
   );
 }
